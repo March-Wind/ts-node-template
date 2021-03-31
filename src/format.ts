ae55d2b1f9077db94aaa15b4b453ef66713a89a3
liuzhiyang <13583106771@163.com>
2021-03-31 17:53:55 +0800

    ts-node模板

--- src/format.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 去掉多余的/
- * @param url 
- */
-const formatURL = (url:string):string => {
-    return url.replace(/\/\//g, '/');
-}
-export default formatURL;
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env ts-node-script
+
+console.log(111);
+interface ASD{
+    as:string
+}
+const asd:ASD = {
+    as:'12222'
+}
+console.log(asd);
